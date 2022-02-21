ce2ac149c52c484935e3f54114cbddc6423157a1
Mark Alfano <mpalfano4@gmail.com>
2022-02-20 19:17:34 -0500

    Got all view selections working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const console = require('console.table');
+const consoleTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,17 +11,17 @@ const db = mysql.createConnection(
         password: 'pass',
         database: 'employee_db'
     },
-    console.log('Connected to the employee_db')
-)
+    console.log(`Connected to the employee_db.`)
+);
 
 
 const select = () => {
-    return inquirer.prompt([
+    inquirer.prompt([
         {
             type: "list",
             name: "menu",
             message: "Select an option",
-            Choices: ["View all departments", "Add a department", "View all roles", "Add a role",
+            choices: ["View all departments", "Add a department", "View all roles", "Add a role",
                 "View all employees", "Add an employee", "Update an employee role"]
         },
 
@@ -45,7 +45,7 @@ const select = () => {
     });
 };
 
-const viewDepartments= () => {
+const viewDepartments = () => {
     const sql = `SELECT department.id AS ID, department.name AS Department FROM department;`;
     db.query(sql, (err, data) => {
         if (err) throw err;
@@ -55,8 +55,53 @@ const viewDepartments= () => {
 }
 
 const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "New department name",
+            name: "department"
+        }
+    ]).then(data => {
+        const newDepartment = data.department;
+        const sql = `INSERT INTO department (name) VALUES ("${newDepartment}");`;
+        db.query(sql, (err, data) => {
+            if (err) throw err;
+            console.table(data);
+            select();
+        });
+    });
+
+};
+
+const viewRoles = () => {
+    const sql = `SELECT role.id AS ID, role.title AS Title, department.name AS department, role.salary FROM role
+    LEFT JOIN department ON role.department_id = department.id;`;
+    db.query(sql, (err, data) => {
+        if (err) throw err;
+        console.table(data)
+        select();
+    })
+}
+
+const addRole = () => {
 
 }
+
+
+const viewEmployees = () => {
+    const sql = `SELECT employee.id AS ID, employee.first_name AS First, employee.last_name AS Last, role.title AS Title,
+    department.name AS department, manager.first_name AS Managers, role.salary as Salary FROM employee 
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id
+    ; `;
+    db.query(sql, (err, data) => {
+        if (err) throw err;
+        console.table(data)
+        select();
+    })
+}
+
 
 
 
