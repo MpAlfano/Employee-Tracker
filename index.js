54e65ee9d10cb956d419af2d1993a1c8682581d1
Mark Alfano <mpalfano4@gmail.com>
2022-02-22 17:32:04 -0500

    Updating with new connect

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const consoleTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 
+//To hide within env file the sensitive data
+require("dotenv").config();
+
+// Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'pass',
-        database: 'employee_db'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     console.log(`Connected to the employee_db.`)
 );
